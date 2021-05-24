4f9eb516ea51f569e6db94a6f92ec1fca171e97f
kyungho <epsoe@naver.com>
2021-05-24 10:25:56 +0900

    location

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
+import * as Location from 'expo-location';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class extends React.Component {
+  getLocation = async () => {
+    try {
+      const response = await Location.requestForegroundPermissionsAsync();
+      console.log(response);
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);
+    } catch (error) {}
+  };
+
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    return <Loading />;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
